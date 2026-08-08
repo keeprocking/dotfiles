9e0b3573194ed44d63a162a3e8b0e3941b508676
Ivan Mukhin <muhin.ivan@gmail.com>
2026-08-08 21:52:05 +0300

    Remove unnecessary zsh options

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -5,10 +5,8 @@ HISTSIZE=5000
 SAVEHIST="$HISTSIZE"
 HISTDUP=erase
 
-setopt appendhistory
 setopt sharehistory
 setopt hist_ignore_space
 setopt hist_ignore_all_dups
 setopt hist_save_no_dups
-setopt hist_ignore_dups
 setopt hist_find_no_dups
